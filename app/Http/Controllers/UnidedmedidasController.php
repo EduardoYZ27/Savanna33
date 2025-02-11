3ad65490ad59542047a7529d91a55417d9796267
EduardoYZ27 <139505103+EduardoYZ27@users.noreply.github.com>
2025-02-11 08:38:14 -0600

    MyC
    Creación de modelos y controladores base

---
+++ app/Http/Controllers/UnidedmedidasController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Unidadmedidas;
+use Illuminate\Http\Request;
+
+class UnidedmedidasController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Unidadmedidas $unidadmedidas)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Unidadmedidas $unidadmedidas)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Unidadmedidas $unidadmedidas)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Unidadmedidas $unidadmedidas)
+    {
+        //
+    }
+}
